ff42b7a9b1b4498645b4f9016aa6613095e848af
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-18 12:51:39 +0200

    Fix AppLogo component path

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -1,13 +1,14 @@
 <template>
-  <header>
-    <a href="#">
+  <header class="container mx-auto flex h-20 items-center px-4">
+    <a class="flex-grow" href="#">
       <app-logo />
     </a>
-    <nav>
-      <ul>
-        <li v-for="link in navLinks" :key="link.text">
-          <a :href="link.url">
-            <span>{{ link.sign }}</span> {{ link.text }}
+    <nav class="mr-8">
+      <ul class="flex">
+        <li v-for="link in navLinks" :key="link.text" class="ml-8 first:ml-0">
+          <a :href="link.url" class="font-mono text-sm">
+            <span>{{ link.sign }}</span>
+            <span>{{ link.text }}</span>
           </a>
         </li>
       </ul>
@@ -17,7 +18,7 @@
 </template>
 
 <script>
-import AppLogo from "@/components/icons/AppLogo.vue";
+import AppLogo from "@/components/Icons/AppLogo.vue";
 import ActionButton from "@/components/Common/ActionButton.vue";
 
 export default {
