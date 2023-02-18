25b1ec79ee763c20e58deef0650a8d09d50154f9
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-18 12:08:34 +0200

    Install TailwindCSS with fonts configuration

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+};
